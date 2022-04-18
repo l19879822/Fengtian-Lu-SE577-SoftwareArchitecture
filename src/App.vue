92281de02b493d4383a21fe88d23d32b4d169851
Fengtian Lu <lft123130@gmail.com>
2022-04-17 23:40:38 -0400

    vue config

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    Hello, welcome to Fengtian's SE-577 course page
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
