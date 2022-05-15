6159c3e77b5dd926fe19f40a6a4e8fee7103c659
Lu, Fengtian <Fengtian_Lu@comcast.com>
2022-05-15 16:00:58 -0400

    put index page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,6 +30,13 @@
     </q-drawer>
 
     <q-page-container>
+      <div id="navlinks">
+        <div id="nav">
+          <router-link to="/">Login</router-link> |
+          <router-link to="/about">About Me</router-link> |
+          <router-link to="/myrepositories">My Repositories</router-link> |
+        </div>
+      </div>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -104,3 +111,26 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+#navlinks {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
