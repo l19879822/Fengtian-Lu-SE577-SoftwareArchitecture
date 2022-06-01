29d0edfaabafe2baa2926761dedecd7c2b6e97c8
Fengtian Lu <lft123130@gmail.com>
2022-06-01 00:27:29 -0400

    add more info regarding authenticated user's information

--- src/pages/AppPages/UserAuth.vue
+++ src/pages/AppPages/UserAuth.vue
@@ -5,12 +5,30 @@
     </q-banner>
     <button @click="authenticate(true)">Authenticated</button>
     <button @click="authenticate(false)">Unauthenticated</button>
-    <div v-if="user">
-      This user is authenticated
-      <p>{{ user }}</p>
-      <p>{{ repoURL }}</p>
+    <div v-if="user" style="margin-top: 50px">
+      <div>This user is authenticated, User information as below:</div>
+      <div class style="margin-top: 20px">
+        Name:
+        <p>{{ user }}</p>
+      </div>
+      <div>
+        User repo url:
+        <p>{{ repoURL }}</p>
+      </div>
+      <div>
+        Last updated at:
+        <p>{{ lastUpdated }}</p>
+      </div>
+      <div>
+        Email:
+        <p>{{ email }}</p>
+      </div>
+      <div>
+        User self introduction:
+        <p>{{ bio }}</p>
+      </div>
     </div>
-    <div v-else>This user is unauthenticated</div>
+    <div v-else style="margin-top: 50px">This user is unauthenticated</div>
   </div>
 </template>
 
@@ -23,6 +41,9 @@ export default defineComponent({
     return {
       user: null,
       repoURL: null,
+      lastUpdated: null,
+      email: null,
+      bio: null,
     };
   },
   methods: {
@@ -37,18 +58,27 @@ export default defineComponent({
           })
           .then((response) => {
             if (response.data.plan !== null) {
-              this.user = response.data.login;
+              this.user = response.data.name;
               this.repoURL = response.data.repos_url;
+              this.lastUpdated = response.data.updated_at;
+              this.email = response.data.email;
+              this.bio = response.data.bio;
             }
             console.log(response);
           })
           .catch(() => {
             this.user = null;
             this.repoURL = null;
+            this.lastUpdated = null;
+            this.email = null;
+            this.bio = null;
           });
       } else {
         this.user = null;
         this.repoURL = null;
+        this.lastUpdated = null;
+        this.email = null;
+        this.bio = null;
       }
     },
   },
